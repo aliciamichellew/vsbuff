67668e924ecfef1be2e490931179d62eeab38730
Lucia Tirta Gunawan <luciatirtag@u.nus.edu>
2023-01-14 16:43:31 +0800

    Fix rollup setup

--- webviews/components/Test.svelte
+++ webviews/components/Test.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script type="module" lang="ts">
 
 </script>
 
--- webviews/pages/Test.ts
+++ webviews/pages/Test.ts
@@ -1,4 +1,4 @@
-import App from "../components/Test.svelte";
+const App = require("../components/Test.svelte");
 
 const app = new App({
   target: document.body,
