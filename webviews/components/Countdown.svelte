e5e266c548000d171811dfc90e62ff527d860f97
Kevin William <kevinwilliam1907@gmail.com>
2023-01-14 21:12:11 +0800

    add countdown

---
+++ webviews/components/Countdown.svelte
@@ -0,0 +1,91 @@
+<script>
+	import { createEventDispatcher } from 'svelte'; 	
+	import { onMount, onDestroy, afterUpdate } from 'svelte';
+	import { tweened } from 'svelte/motion';
+	export let countdown = 0;
+	const dispatch = createEventDispatcher();
+	
+	let timer = null;
+	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reverse();
+	$: minutes = Math.floor(countdown / 60);
+	$: seconds = countdown % 60;
+
+	let firstH = tweened((9 - Math.floor(Math.floor(countdown / 60) / 10)) * 128, { duration: 300});
+	let firstL = tweened((9 - Math.floor(Math.floor(countdown / 60) % 10)) * 128, { duration: 300});
+	let secondH = tweened((9 - Math.floor(countdown % 60 / 10)) * 128, { duration: 300 });
+	let secondL = tweened((9 - Math.floor(countdown % 60 % 10)) * 128, { duration: 300 });
+
+	const val = tweened(0, { duration: 500 });
+	
+	onMount(() => {
+		timer = setInterval(() => {
+			countdown -= 1;
+	  }, 1000);
+	})
+	
+	afterUpdate(() => {
+		firstH.set((9 - Math.floor(minutes / 10)) * 128);
+		firstL.set((9 - Math.floor(minutes % 10)) * 128);
+		secondH.set((9 - Math.floor(seconds / 10)) * 128);
+		secondL.set((9 - Math.floor(seconds % 10)) * 128);		
+	})
+	
+	onDestroy(() => {
+		if (timer) {
+			clearInterval(timer);
+		}
+	})
+	
+	$: {
+		if (countdown === 0) {
+			clearInterval(timer);
+			timer = null;
+			dispatch('completed');
+		}
+	}
+</script>
+
+<style>
+	ul {
+		display: inline-block;
+		list-style: none;
+		padding-left: 0;
+		height: 94px;
+		overflow: hidden;
+	}
+
+	.num {
+    font-size: 94px;
+	}
+</style>
+<ul>
+	{#each numbers as num, i}
+  <li class="num" style="transform: translateY(-{$firstH}px);">
+   <span>{num}</span>
+  </li>
+  {/each}
+</ul>
+
+<ul>
+	{#each numbers as num, i}
+  <li class="num" style="transform: translateY(-{$firstL}px);">
+   <span>{num}:</span>
+  </li>
+  {/each}
+</ul>
+
+<ul>
+	{#each numbers as num, i}
+  <li class="num" style="transform: translateY(-{$secondH}px);">
+   <span>{num}</span>
+  </li>
+  {/each}
+</ul>
+
+<ul>
+	{#each numbers as num, i}
+  <li class="num" style="transform: translateY(-{$secondL}px);">
+   <span>{num}</span>
+  </li>
+  {/each}
+</ul>
--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,3 +1,26 @@
-<script lang="ts">
+<script>
+	import { fly } from 'svelte/transition'
+	import Countdown from './Countdown.svelte';
+	let done = false;
 </script>
-<div>hello2</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.inner {
+		text-align: center;
+	}
+</style>
+
+<div class="container">
+	<div class="inner">
+		<Countdown countdown={360} on:completed="{() => done = true}" />
+
+		{#if done}
+			<h2 in:fly>時間到！</h2>	
+		{/if}		
+	</div>
+</div>
