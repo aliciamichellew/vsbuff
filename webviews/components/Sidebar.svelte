aa6a41d2781a6885fb2e5798a7fa20bc61bd90a8
Kevin William <kevinwilliam1907@gmail.com>
2023-01-14 23:10:08 +0800

    Add dumbells

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -3,6 +3,9 @@
 	import Countdown from './Countdown.svelte';
 	let done = false;
     let userInput = 0
+    let images = ['https://i.ibb.co/Z2S3zrS/1.png' , 'https://i.ibb.co/7zYcMgc/2.png' , 'https://i.ibb.co/cJThrmp/3.png' , 'https://i.ibb.co/dtf805F/4.png' , 'https://i.ibb.co/wNSj4Ds/8.png', 'https://i.ibb.co/qmSKDj1/7.png' , 'https://i.ibb.co/3cxCncD/5.png' , 'https://i.ibb.co/tbYdv5R/6.png' ,  'https://i.ibb.co/YQnMHnv/9.png', 'https://i.ibb.co/8McTXH7/10.png', 'https://i.ibb.co/VBc2YVP/11.png', 'https://i.ibb.co/DgT1SKb/12.png',
+'https://i.ibb.co/Lh76VKS/13.png' , 'https://i.ibb.co/Hn9FvGb/14.png' , 'https://i.ibb.co/TgG1L2D/15.png'];
+;   let count = 0
 </script>
 
 <style>
@@ -32,5 +35,15 @@
 		{#if done}
 			<h2 in:fly>You have been working hard! Time to exercise!</h2>	
 		{/if}		
+
+        <img src={images[count % 15]} />
+        <br />
+        <br />
+        <div>You have lifted the dumbbells {Math.floor(count / 14)} times ({Math.floor(1000 / 14)} lift = 1 calorie)</div>
+        <br />
+        <button on:click={() => {count++;}}>Lift the Dumbbells</button>
+        <br />
+        <br />
+        <div>You have burn {Math.floor(count * 0.001)} calories from <strike>clicking the mouse</strike> lifting the dumbbells.</div>
 	</div>
 </div>
